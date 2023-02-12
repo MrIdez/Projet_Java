e8fa47188a341a2e944a95663ff5f534f61db729
Guillaume BARON <guillaume.baron62@orange.fr>
2023-02-12 17:33:53 +0100

    On commence Bataille.java et le pseudo
    On marque qu'elle que idée sur le pseudo et on le met en Java
    
    Signed-off-by: Guillaume BARON <guillaume.baron62@orange.fr>

---
+++ Bataille/src/jeux/Bataille.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package jeux;
+
+import joueur.JoueurBridge;
+import paquetDeCarte.PaquetDeCarteBridge;
+import paquetDeCarte.jeuDeCarteBridge;
+
+/**
+ * 
+ * @author guillaume
+ *
+ */
+public class Bataille {
+	private JoueurBridge j1;
+	private JoueurBridge j2;
+	private jeuDeCarteBridge pioche ;
+	private PaquetDeCarteBridge carteEnJeu;
+	public Bataille() {
+		// TODO Auto-generated constructor stub
+	}
+	
+
+}
